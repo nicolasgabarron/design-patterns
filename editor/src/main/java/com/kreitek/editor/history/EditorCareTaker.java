0c78cdd3a4f4b8d43a62751270d4144fbd4e48ca
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-04-07 13:42:22 +0200

    feat: Clase CareTaker.

--- editor/src/main/java/com/kreitek/editor/history/EditorCareTaker.java
+++ editor/src/main/java/com/kreitek/editor/history/EditorCareTaker.java
@@ -1,4 +1,38 @@
 package com.kreitek.editor.history;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EditorCareTaker {
+    // Propiedades.
+    private List<EditorMemento> mementoList = new ArrayList<>();
+
+    /**
+     * Método que introduce en la lista de mementos (pila de "snapshots") un nuevo
+     * memento.
+     *
+     * @param memento Memento a introducir en la lista.
+     */
+    public void push(EditorMemento memento){
+        mementoList.add(memento);
+    }
+
+    /**
+     * Método que recupera y devuelve el último elemento de la lista de mementos
+     * (pila de "snapshots") y posteriormente lo elimina.
+     *
+     * @return Último memento disponible. En caso de no haber mementos, devuelve NULL.
+     */
+    public EditorMemento pop(){
+        if(mementoList.size()>0){
+            final int LAST_INDEX = mementoList.size()-1; // Índice del último elemento de la lista.
+
+            EditorMemento mementoPop = mementoList.get(LAST_INDEX);
+            mementoList.remove(LAST_INDEX);
+
+            return mementoPop;
+        }
+
+        return null;
+    }
 }
