3f8ebe417cfa97a81855025e6ce7b315ecb083e7
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-04-09 20:51:55 +0200

    feat: Definición de estrategia del programa.

--- editor/src/main/java/com/kreitek/editor/Application.java
+++ editor/src/main/java/com/kreitek/editor/Application.java
@@ -1,9 +1,22 @@
 package com.kreitek.editor;
 
+import com.kreitek.editor.strategies.JSONStrategy;
+import com.kreitek.editor.strategies.TextStrategy;
+import com.kreitek.editor.utils.printers.DocumentPrinter;
+
 public class Application {
 
     public static void main(String[] args) {
         EditorFactory editorFactory = new EditorFactory();
+        DocumentPrinter documentPrinter = DocumentPrinter.getInstance();
+
+        // Defino la estrategia de ejecución del programa.
+        if (args[0].equalsIgnoreCase("JSON")) {
+            documentPrinter.setPrintStrategy(new JSONStrategy());
+        } else {
+            documentPrinter.setPrintStrategy(new TextStrategy());
+        }
+
         Editor editor = editorFactory.getEditor();
         editor.run();
     }
