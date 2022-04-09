4463eedcb3dc286aa9c8da7f547a50d438994b06
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-04-09 20:49:27 +0200

    feat: Funcionalidad JSONStrategy.

--- editor/src/main/java/com/kreitek/editor/strategies/JSONStrategy.java
+++ editor/src/main/java/com/kreitek/editor/strategies/JSONStrategy.java
@@ -4,12 +4,26 @@ import com.kreitek.editor.utils.printers.TextPrinter;
 
 import java.util.List;
 
-public class JSONStrategy implements PrintStrategy{
+public class JSONStrategy implements PrintStrategy {
     // Propiedades.
     private TextPrinter textPrinter = TextPrinter.getInstance();
 
     @Override
     public void printDocument(List<String> document) {
+        // Cabecera JSON
+        textPrinter.printLnToConsole("{");
+        textPrinter.printLnToConsole("\t\"doc\":[");
 
+        // Cuerpo JSON
+        for (int index = 0; index < document.size(); index++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("{\"line\":\"" + index + "\",");
+            stringBuilder.append("\"text\":\"" + document.get(index) + "\"}");
+            textPrinter.printLnToConsole("\t\t" + stringBuilder.toString());
+        }
+
+        // Pie JSON
+        textPrinter.printLnToConsole("\t]");
+        textPrinter.printLnToConsole("}");
     }
 }
