86ccd8f79bc9580cc316449c4daa4dbd8adebe19
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-04-09 20:42:55 +0200

    feat: Funcionalidad TextStrategy.

--- editor/src/main/java/com/kreitek/editor/strategies/TextStrategy.java
+++ editor/src/main/java/com/kreitek/editor/strategies/TextStrategy.java
@@ -5,9 +5,18 @@ import com.kreitek.editor.utils.printers.TextPrinter;
 import java.util.List;
 
 public class TextStrategy implements PrintStrategy{
+    // Propiedades.
+    private TextPrinter textPrinter = TextPrinter.getInstance();
 
     @Override
     public void printDocument(List<String> document) {
-
+        for (int index = 0; index < document.size(); index++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("[");
+            stringBuilder.append(index);
+            stringBuilder.append("] ");
+            stringBuilder.append(document.get(index));
+            textPrinter.printLnToConsole(stringBuilder.toString());
+        }
     }
 }
